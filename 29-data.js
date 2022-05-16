28de6f2317702f810be7484e7724b2d059eb3b62
epvillar <villar_esteban_pedro@hotmail.com>
2022-05-16 15:18:03 -0300

    'first'

---
+++ 29-data.js
@@ -0,0 +1,69 @@
+const people = [
+    {
+      img: "./29-slide1.jpg",
+      name: "love",
+      job: "product manager",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+    },
+    {
+      img: "29-slide2.jpg",
+      name: "watch it!",
+      job: "developer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+    {
+      img:
+        "29-slide3.jpg",
+      name: "flowers",
+      job: "designer",
+      text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+    },
+    {
+    img: "./29-slide4.jpg",
+    name: "cold",
+    job: "product manager",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img: "29-slide5.jpg",
+    name: "dispair",
+    job: "developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img:
+      "29-slide6.jpg",
+    name: "bananas!",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "./29-slide7.jpg",
+    name: "deceit!",
+    job: "product manager",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img: "29-slide8.jpg",
+    name: "help!",
+    job: "developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img:
+      "29-slide9.jpg",
+    name: "kiss me!",
+    job: "designer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+  img: "./29-slide10.jpg",
+  name: "control me!",
+  job: "product manager",
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+},
+
+
+  ];
+  
+  export default people;
